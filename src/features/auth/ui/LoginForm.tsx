dfafd4bbbaba7acb606b584af1edef639d603ced
Bahodir Nabijanov <bahodirnabijanov782@gmail.com>
2026-02-12 12:26:23 +0500

    login page ui

--- src/features/auth/ui/LoginForm.tsx
+++ src/features/auth/ui/LoginForm.tsx
@@ -1,34 +1,35 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import logo from "../../../shared/assets/logoo.png";
+import { Eye, EyeClosed } from "lucide-react";
 
 const LoginForm = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="flex items-center justify-center min-h-[500px]">
-      <div className="bg-primary p-8 sm:p-10 rounded-2xl shadow-[0_20px_50px_rgba(0,0,0,0.1)] w-[460px] max-w-full mx-4 border border-gray-50">
+    <div className="flex items-center justify-center">
+      <div className="bg-primary p-8 sm:p-10 rounded-2xl shadow-[0_20px_50px_rgba(0,0,0,0.1)] w-115 max-w-full mx-4">
         <form onSubmit={(e) => e.preventDefault()} className="flex flex-col">
-          {/* Logo va Brend nomi */}
           <div className="flex flex-col items-center mb-8">
-            <div className="bg-main p-3 rounded-xl mb-4 shadow-lg">
+            <div className="bg-primarydark p-3 rounded-xl mb-4 shadow-lg">
               <img
                 src={logo}
                 alt="Elchi Logo"
                 className="h-10 sm:h-12 w-auto"
               />
             </div>
-            <h1 className="text-2xl sm:text-3xl font-bold text-gray-800 tracking-tight">
+            <h1 className="text-2xl sm:text-3xl font-bold text-primarydark tracking-tight">
               Elchi <span className="text-main">Pochta</span>
             </h1>
           </div>
 
           <div className="space-y-5">
-            {/* Telefon raqami */}
             <div>
               <label className="block text-xs font-semibold text-gray-500 uppercase tracking-wider mb-1 ml-1">
                 Telefon raqam
               </label>
               <input
                 type="text"
-                className="w-full h-12 px-4 text-gray-700 bg-gray-50 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-main/20 focus:border-main transition-all duration-200"
+                className="w-full h-12 px-4 text-maindark bg-gray-50 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-main focus:border-transparent transition-all duration-200"
                 placeholder="+998 90 123 45 67"
               />
             </div>
@@ -40,17 +41,31 @@ const LoginForm = () => {
                   Parol
                 </label>
               </div>
-              <input
-                type="password"
-                className="w-full h-12 px-4 text-gray-700 bg-gray-50 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-main/20 focus:border-main transition-all duration-200"
-                placeholder="••••••••"
-              />
+              <div className="flex justify-between items-center w-full h-12 px-4 text-maindark bg-gray-50 border border-gray-200 rounded-xl focus-within:ring-2 focus-within:ring-main focus-within:border-transparent transition-all duration-200">
+                <input
+                  name="password"
+                  type={show ? "text" : "password"}
+                  placeholder="••••••••"
+                  className="w-full outline-none bg-transparent"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShow(!show)}
+                  className="cursor-pointer focus:outline-none text-gray-400 hover:text-main transition-colors"
+                >
+                  {show ? (
+                    <Eye size={20} />
+                  ) : (
+                    <EyeClosed size={20} />
+                  )}
+                </button>
+              </div>
             </div>
 
             {/* Kirish tugmasi */}
             <button
               type="submit"
-              className="bg-main w-full h-12 text-primary rounded-xl font-bold text-lg shadow-lg shadow-main/30 hover:shadow-main/40 hover:-translate-y-0.5 active:translate-y-0 transition-all duration-200 mt-4"
+              className="bg-primarydark w-full h-12 text-primary rounded-xl font-bold text-lg shadow-lg shadow-main/30 hover:shadow-main/40 hover:-translate-y-0.5 active:translate-y-0 transition-all duration-200 mt-4 cursor-pointer"
             >
               Tizimga kirish
             </button>
